bb1270e31cba46cff819f8be5f0ba5e2ea301106
way2muchnoise <jens.baetens@live.be>
2017-12-15 11:06:48 +0100

    check-in day15

---
+++ day15/part2.py
@@ -0,0 +1,30 @@
+f = open('input.txt', 'r')
+lines = f.readlines()
+f.close()
+
+factor_a = 16807
+factor_b = 48271
+max_int = 2147483647
+
+start_a = int(lines[0].replace('\n', '').split(' ')[-1])
+a = int((start_a * factor_a) % max_int)
+while a % 4 != 0:
+    a = int((a * factor_a) % max_int)
+start_b = int(lines[1].replace('\n', '').split(' ')[-1])
+b = int((start_b * factor_b) % max_int)
+while b % 8 != 0:
+    b = int((b * factor_b) % max_int)
+
+matches = 0
+for i in range(0, 5000000):
+    lower_a = bin(a)[-16:]
+    lower_b = bin(b)[-16:]
+    if lower_a == lower_b:
+        matches += 1
+    a = int((a * factor_a) % max_int)
+    while a % 4 != 0:
+        a = int((a * factor_a) % max_int)
+    b = int((b * factor_b) % max_int)
+    while b % 8 != 0:
+        b = int((b * factor_b) % max_int)
+print matches
